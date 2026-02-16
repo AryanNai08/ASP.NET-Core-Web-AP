6701ca1a63dad382c246c5df2596a265161aa88a
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-02-16 14:29:56 +0530

    Created first end point

---
+++ CollegeApi/Controllers/StudentContoller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentContoller : ControllerBase
+    {
+
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "Aryan Nai";
+        }
+    }
+}
