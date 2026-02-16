db058fe529533d54b9495512e16f20fbdbd724ae
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-02-16 14:44:27 +0530

    Added list of students to display in endpoints

--- CollegeApi/Controllers/StudentContoller.cs
+++ CollegeApi/Controllers/StudentContoller.cs
@@ -1,17 +1,36 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CollegeApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CollegeApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+    [Route("api/[controller]")] //Defines the base URL route
+    [ApiController]//Marks the class as a Web API controller
     public class StudentContoller : ControllerBase
     {
-
+        //request method id endpoint
         [HttpGet]
-        public string GetStudent()
+
+        //first end point
+        public IEnumerable<Student> GetStudent()
         {
-            return "Aryan Nai";
+            return new List<Student>(){
+            new Student
+            {
+                Id = 1,
+                Studentname = "Aryan Nai",
+                Email = "aryannai841@gmail.com",
+                Address = "Ahmedabad,Gujarat"
+            },
+            new Student
+            {
+                Id = 2,
+                Studentname = "Kartik Ahir",
+                Email = "kartik@gmail.com",
+                Address = "Surat,Gujarat"
+            } };
+
+
         }
     }
 }
---
+++ CollegeApi/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace CollegeApi.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public String Studentname { get; set; }
+        public String Email { get; set; }
+        public String Address { get; set; }
+    }
+}
