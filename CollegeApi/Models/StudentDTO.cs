042db7639574369169ca5e4e11e2765b7ce60849
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-02-17 10:44:18 +0530

    Added validation constraint in studendto file

--- CollegeApi/Models/StudentDTO.cs
+++ CollegeApi/Models/StudentDTO.cs
@@ -1,11 +1,18 @@
-﻿namespace CollegeApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollegeApi.Models
 {
     public class StudentDTO
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage="Student name is required")]
         public String Studentname { get; set; }
+
+        [EmailAddress(ErrorMessage = "Enter valid Email")]
         public String Email { get; set; }
+
+        [Required (ErrorMessage = "Student Address is required")]
         public String Address { get; set; }
     }
 }
