b20f99052fbf050fb404acebdffa45b0a7b73dc3
Nai Aryan <130205139+AryanNai08@users.noreply.github.com>
2026-02-19 11:00:18 +0530

    Configure automapper based on diffrent scenario

--- CollegeApi/Configurations/AutoMapperConfig.cs
+++ CollegeApi/Configurations/AutoMapperConfig.cs
@@ -7,8 +7,19 @@ namespace CollegeApi.Configurations
     public class AutoMapperConfig : Profile
     {
         public AutoMapperConfig()
-        { 
-            CreateMap<Student,StudentDTO>().ReverseMap();
+        {
+
+            //congiguration for diffrent property name 
+            //CreateMap<StudentDTO,Student>().ForMember(x => x.Studentname,opt=>opt.MapFrom(x=>x.Name)).ReverseMap();
+
+
+            //congiguration for ignoring some property
+            //CreateMap<StudentDTO, Student>().ReverseMap().ForMember(x => x.Studentname, Opt => Opt.Ignore());
+
+            //congigurattion for transforming some propery
+            //CreateMap<StudentDTO, Student>().ReverseMap().AddTransform<string>(n=>string.IsNullOrEmpty(n)?"No address found":n);
+
+            CreateMap<StudentDTO, Student>().ReverseMap();
         }
     }
 }
